f843a27d8507eed06abcefeb292e8b9adc04221c
Alanypm <81442841+Alanypm@users.noreply.github.com>
2021-04-03 19:26:58 -0600

    Delete init.js

--- js/init.js
+++
@@ -1,9 +0,0 @@
-// @ts-ignore
-firebase.initializeApp({
-  apiKey: "AIzaSyBgel6aRgVSviRJO9afQNa7gGqO7oM8wMo",
-  authDomain: "pruebapwa-iot.firebaseapp.com",
-  projectId: "pruebapwa-iot",
-  storageBucket: "pruebapwa-iot.appspot.com",
-  messagingSenderId: "847609927106",
-  appId: "1:847609927106:web:612d4a198f954ce1be325b"
-});
